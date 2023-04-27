239fc42a422fabf952c808e08bcfba1b1281fe73
Gabriel Bustos Flores <bustosfloresgabriel@gmail.com>
2023-04-27 13:48:05 -0600

    added code to sort coutnies by score

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+// import React, { useEffect, useRef } from 'react';
 
-const {tableau} = window;
+// const {tableau} = window;
 
-export const Dashboard =()=> {
-  const ref = useRef(null);
-  const url =  "https://public.tableau.com/views/Dashboard_HMEA/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
-  function initViz(){
-    new tableau.Viz(ref.current,url);
-  }
-  useEffect(() => {
-    initViz()
-  }, []);
+// export const Dashboard =()=> {
+//   const ref = useRef(null);
+//   const url =  "https://public.tableau.com/views/Dashboard_HMEA/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
+//   function initViz(){
+//     new tableau.Viz(ref.current,url);
+//   }
+//   useEffect(() => {
+//     initViz()
+//   }, []);
 
   
-  return (
-    <div style={{width:'70%', margin:'auto'}}> </div>
-  );
-}
+//   return (
+//     <div style={{width:'70%', margin:'auto'}}> </div>
+//   );
+// }
 
 
