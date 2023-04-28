a52aa10751475a52b43e6aff6d2eb2f8b32b1f3e
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2023-04-28 03:55:57 -0600

    Clean up unneeded files

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Pie from "./components/Pie";
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Counties from "./components/Counties";
-// import { Dashboard } from "./components/Dashboard";
-
 
 export default function App() {
   const API_URI = "http://localhost:5555/counties";
@@ -21,23 +19,24 @@ export default function App() {
       console.log(error);
     }
   };
-  
-  const countiesSortedDescending = [...counties].sort((a, b) => b.score - a.score);
+
+  const countiesSortedDescending = [...counties].sort(
+    (a, b) => b.score - a.score
+  );
   console.log(countiesSortedDescending);
 
-  const {tableau} = window;
+  const { tableau } = window;
 
   const ref = useRef(null);
-  const url =  "https://public.tableau.com/views/Dashboard_HMEA/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
-  function initViz(){
+  const url =
+    "https://public.tableau.com/views/Dashboard_HMEA/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
+  function initViz() {
     let viz = window.tableau.VizManager.getVizs()[0];
 
-        if (viz) {
-
-            viz.dispose();
-
-        }
-    new tableau.Viz(ref.current,url);
+    if (viz) {
+      viz.dispose();
+    }
+    new tableau.Viz(ref.current, url);
   }
 
   useEffect(() => {
@@ -58,40 +57,36 @@ export default function App() {
       colour: `hsl(${rand(360)}, ${rand(50) + 50}%, ${rand(30) + 20}%)`,
     });
   };
-  
-  
-
 
   return (
     <>
-    <section className="nav-container">
+      <section className="nav-container">
         <h1>Dynamic Data</h1>
-    </section>
-    <section className="main-dashboard">
-      <div className="main-dashboard-container">
-        <header>
-          <h1>Dashboard</h1>
-        </header>
-        <div className="data-container">
-          <div class='counties-element-container'>
-            <Counties countiesSortedDescending={countiesSortedDescending} handleScore={handleScore} />
-          </div>
-          <div className="number-container">
-            <Pie percentage={score.percentage} colour={score.colour} />
-            
+      </section>
+      <section className="main-dashboard">
+        <div className="main-dashboard-container">
+          <header>
+            <h1>Dashboard</h1>
+          </header>
+          <div className="data-container">
+            <div class="counties-element-container">
+              <Counties
+                countiesSortedDescending={countiesSortedDescending}
+                handleScore={handleScore}
+              />
+            </div>
+            <div className="number-container">
+              <Pie percentage={score.percentage} colour={score.colour} />
+            </div>
           </div>
         </div>
+      </section>
+      <div ref={ref} style={{ width: "70%", margin: "auto" }}>
+        {" "}
       </div>
-    </section>
-        <div ref={ref} style={{width:'70%', margin:'auto'}}> </div>
-    <section>
-      <div>
-      </div>
-
-    </section>
+      <section>
+        <div></div>
+      </section>
     </>
   );
 }
-
-
-
